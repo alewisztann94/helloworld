9fd9ba90b8d856642260a603c7886db781b48990
David Glance <david.glance@gmail.com>
2018-03-25 17:05:04 +0800

    Testing the changing of file and automatic build

--- HelloWorld_test.py
+++ HelloWorld_test.py
@@ -6,6 +6,19 @@ class MyTestCase(unittest.TestCase):
         hw = HelloWorld()
         self.assertEqual(hw.message, 'Hello world!')
 
+    def test_isgt_lt(self):
+        hw = HelloWorld()
+        self.assertFalse(hw.isgt(3, 4))
+
+    def test_isgt_gt(self):
+        hw = HelloWorld()
+        self.assertTrue(hw.isgt(4, 3))
+
+    def test_isgt_notnumber(self):
+        hw = HelloWorld()
+        self.assertRaises(ValueError, hw.isgt, "4", "5")
+
+
 
     # def test_demo(self):
     #    self.fail()
