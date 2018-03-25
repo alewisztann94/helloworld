867b999acdc23d1f2ad49fdda439067cf533cd3d
David Glance <david.glance@gmail.com>
2018-03-25 18:59:11 +0800

    Testing the changing of file and automatic build

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,3 +1,23 @@
+import numbers
+
 class HelloWorld:
     def __init__(self):
         self.message = 'Hello world!'
+
+    '''
+    Given two numbers return True if a > b
+    '''
+    def isgt(self, a, b):
+
+        if not isinstance(a, numbers.Number):
+            raise ValueError
+
+        if not isinstance(b, numbers.Number):
+            raise ValueError
+
+
+        if a > b:
+            return True
+        else:
+            return False
+
